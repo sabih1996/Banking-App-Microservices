de7338a4e69a1c99225d8e4665254558ad165502
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2025-10-06 16:30:58 +0300

    Initial commit

---
+++ src/test/java/com/example/starth2/Starth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Starth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
