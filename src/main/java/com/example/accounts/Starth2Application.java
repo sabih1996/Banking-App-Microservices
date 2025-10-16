53c34ac5be7fda97e39637822ef9928d61d27fc7
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2025-10-16 15:19:09 +0300

    origin/main

--- src/main/java/com/example/accounts/Starth2Application.java
+++ src/main/java/com/example/accounts/Starth2Application.java
@@ -1,4 +1,4 @@
-package com.example.starth2;
+package com.example.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,69 @@
-CREATE TABLE IF NOT EXISTS `customer` (
-    `customer_id` int AUTO_INCREMENT  PRIMARY KEY,
-     `name` varchar(100) NOT NULL,
-    `email` varchar(100) NOT NULL,
-    `mobile_number` varchar(20) NOT NULL,
-    `created_at` date NOT NULL,
-    `created_by` varchar(20) NOT NULL,
-    `updated_at` date DEFAULT NULL,
-    `updated_by` varchar(20) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS customer
+(
+    customer_id
+    INT
+    GENERATED
+    BY
+    DEFAULT AS
+    IDENTITY
+    PRIMARY
+    KEY,
+    name
+    VARCHAR
+(
+    100
+) NOT NULL,
+    email VARCHAR
+(
+    100
+) NOT NULL,
+    mobile_number VARCHAR
+(
+    20
+) NOT NULL,
+    created_at DATE NOT NULL,
+    created_by VARCHAR
+(
+    20
+) NOT NULL,
+    updated_at DATE DEFAULT NULL,
+    updated_by VARCHAR
+(
+    20
+) DEFAULT NULL
     );
 
-CREATE TABLE IF NOT EXISTS `accounts` (
-    `customer_id` int NOT NULL,
-    `account_number` int AUTO_INCREMENT  PRIMARY KEY,
-    `account_type` varchar(100) NOT NULL,
-    `branch_address` varchar(200) NOT NULL,
-    `created_at` date NOT NULL,
-    `created_by` varchar(20) NOT NULL,
-    `updated_at` date DEFAULT NULL,
-    `updated_by` varchar(20) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS accounts
+(
+    account_number
+    INT
+    GENERATED
+    BY
+    DEFAULT AS
+    IDENTITY
+    PRIMARY
+    KEY,
+    customer_id
+    INT
+    NOT
+    NULL,
+    account_type
+    VARCHAR
+(
+    100
+) NOT NULL,
+    branch_address VARCHAR
+(
+    200
+) NOT NULL,
+    created_at DATE NOT NULL,
+    created_by VARCHAR
+(
+    20
+) NOT NULL,
+    updated_at DATE DEFAULT NULL,
+    updated_by VARCHAR
+(
+    20
+) DEFAULT NULL
     );
