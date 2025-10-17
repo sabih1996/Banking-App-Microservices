7b9fc4121099d5499ebc1be951bee0c201f0d2fb
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2025-10-17 11:27:35 +0300

    added docs for accounts service endpoint

--- src/main/java/com/example/accounts/Starth2Application.java
+++ src/main/java/com/example/accounts/Starth2Application.java
@@ -2,8 +2,11 @@ package com.example.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+
 public class Starth2Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/accounts/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/accounts/service/impl/AccountServiceImpl.java
@@ -32,11 +32,8 @@ public class AccountServiceImpl implements IAccountsService {
         if (optionalCustomer.isPresent()) {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber " + customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
-
     }
 
     /**
@@ -51,8 +48,6 @@ public class AccountServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("anonymous");
         return newAccount;
     }
 
